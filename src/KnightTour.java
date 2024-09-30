d9698e614598d45ff22812333f6403a0d3371e80
Emma du Maurier <emmadumaurier@gmail.com>
2024-09-30 14:58:53 +0100

    initial commit

---
+++ src/KnightTour.java
@@ -0,0 +1,65 @@
+public class KnightTour
+{
+    // Set the base size for the chess board
+    private static final int boardSize = 8;
+
+    // Constant array for all the movements the knight piece can move
+    private static final int[][] knightMoves = {
+            {-2, -1}, {-1, -2}, {1, -2}, {2, -1},
+            {2, 1}, {1, 2}, {-1, 2}, {-2, 1}
+    };
+
+    public static void main(String[] args)
+    {
+        // Create a 2D array for our chessboard
+        int[][] chessBoard = new int [boardSize][boardSize];
+
+        // Set the initial starting point for the knight and the move counter
+        int currentPosX = 0, currentPosY = 0, moveCounter = 1;
+
+        // Place the counter value to the current knight location in the chessboard array
+        chessBoard[currentPosX][currentPosY] = moveCounter;
+
+        int[] nextMove = findNextMove(chessBoard, currentPosX, currentPosY);
+        System.out.printf("Next move: X: %d, Y: %d%n", nextMove[0], nextMove[1]);
+    }
+
+    // Checks if the position is on the board, and if the position has not yet been visited
+    private static boolean isValidMove(int[][] board, int xPos, int yPos) {
+        return (xPos >= 0 && xPos < boardSize && yPos >= 0 && yPos < boardSize && board[xPos][yPos] == 0);
+    }
+
+    // Determine how many available positions there are from a provided position
+    private static int availablePositions(int[][] board, int xPos, int yPos)
+    {
+        int count = 0;
+        for (int[] move : knightMoves) {
+            int newX = xPos + move[0];
+            int newY = yPos + move[1];
+            if (isValidMove(board, newX, newY)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Returns the best choice for the next move based on the minimum number of available positions
+    private static int[] findNextMove(int[][] board, int xPos, int yPos) {
+        int minMoves = Integer.MAX_VALUE;
+        int[] bestMove = {-1, -1};
+
+        for (int[] move : knightMoves) {
+            int newX = xPos + move[0];
+            int newY = yPos + move[1];
+            if (isValidMove(board, newX, newY)) {
+                int moves = availablePositions(board, newX, newY);
+                if (moves < minMoves) {
+                    minMoves = moves;
+                    bestMove[0] = newX;
+                    bestMove[1] = newY;
+                }
+            }
+        }
+        return bestMove;
+    }
+}
